7166f722894ac62eec6247274af7b872ff26d019
AirKeyooo <airkeyooo@gmail.com>
2026-06-16 18:06:27 +0200

    Finished prediction display, fixed SERIOUS issue with reward info loading

--- loader.py
+++ loader.py
@@ -107,8 +107,8 @@ class Code:
         self.code = code
         self.logger.debug(f"Creating Code object for code: {code}")
         if timestamp:
-            self.tsobj = timestamp
-            self.timestamp = timestamp.isoformat(timespec="seconds")
+            self.tsobj = timestamp.astimezone()
+            self.timestamp = self.tsobj.isoformat(timespec="seconds")
             self.logger.debug(f"Date loaded from given parameter: {self.tsobj}")
         else:
             timediff = datetime.now().astimezone()-timedelta(seconds=time)
@@ -120,7 +120,7 @@ class Code:
         self.isitme = player in nicknames
         self.logger.debug(f"Loaded informations. Player: {self.player} ({"Me"if self.isitme else "Not me"}); Re-write time: {self.time}")
     def __eq__(self,other):
-        return self.code == other.code and self.timestamp == other.timestamp and self.player == other.player and self.time == other.time and self.isitme == other.isitme
+        return self.code == other.code and self.tsobj == other.tsobj and self.player == other.player and self.time == other.time and self.isitme == other.isitme
     def to_csv(self):
         self.logger.debug("Exporting data to format expected by CSV class")
         return (self.code,self.player,self.time,self.timestamp,self.isitme)
@@ -148,6 +148,15 @@ class AnarchiaGG(Minecraft):
         self.my_wins = 0
         self.last_code = None
         self.last_code_ts = None
+    def read_messages_after_code(self,n=1)->tuple:
+        lastlines=self.read_raw_messages(n=n)
+        lines=[]
+        for line in reversed(lastlines):
+            if "Przepisz kod " in line and " aby otrzymać nagrodę!" in line:
+                break
+            else:
+                lines.append(line)
+        return tuple(reversed(lines))
     def get_code(self,n=1)->str:
         # Search for code to copy
         lastlines=self.read_raw_messages(n=n)
@@ -165,7 +174,8 @@ class AnarchiaGG(Minecraft):
         return None
     def get_winner(self,n=1):
         # Search for winner info to save it
-        lastlines=self.read_raw_messages(n=n)
+        #lastlines=self.read_raw_messages(n=n) OLD VERSION
+        lastlines=self.read_messages_after_code(n=n)
         for l in lastlines:
             if "Gracz " in l and " jako pierwszy przepisał kod w czasie " in l and "s i otrzymał(a) 3 Klucze AFK!" in l and self.last_code and self.last_code_ts:
                 splitted = l.split(" jako pierwszy przepisał kod w czasie ")
@@ -195,7 +205,7 @@ class AnarchiaGG(Minecraft):
         self.logger.debug(f"Date from last code: {last_code}")
         timediff=last_code-self.wins[-2].tsobj
         self.logger.debug(f"Time difference between last and second last code: {timediff}")
-        time_since_last_code=datetime.now()-last_code
+        time_since_last_code=datetime.now().astimezone()-last_code
         self.logger.debug(f"Time since last code: {time_since_last_code}")
         predicted_time=timediff-time_since_last_code
         self.logger.debug(f"Predicted time to next code: {predicted_time}")
@@ -382,7 +392,7 @@ class Config:
         if fullpath.exists():
             self.logger.error("Config file already exists!")
             exit(rtn.EX_CANTCREAT)
-        DEFAULT_CONFIG_FILE=f'''# MC Code Copier default config file\n# Program maintainer: AirKeyooo <airkeyooo@gmail.com>\n# Generated: {datetime.now().astimezone().strftime("%d.%m.%Y %H:%M:%S %Z")}\n\n# Add path to your latest.log file\nlog_file: /path/to/latest.log\n\n# How many lines program should read. More lines = improved efficiency, but higher CPU and disk usage\nread_lines: 2\n\n# Should program send notifications about new codes?\n# WARNING: Works only on Linux and ends with error on any other OS!\nsend_notifications: false\n\n# Suggest when user should send code to don't look suspicious\n# Give value in seconds. If you don't want to use this function, comment it or set value to 0\nsuggest_timeout: 5\n\n# Should program automatically copy received code to the clipboard?\n# WARNING: Requires pyperclib module, which can be installed using: `pip install pyperclip`\ncopy_to_clipboard: false\n\n# Save results to .csv file\n# If you don't want to use this function, comment line below\nsave_to_csv: /path/to/file.csv\n\n# Set all nicknames that are yours\n# If you don't want to set your nicknames, comment/remove whole section below\nnicknames:\n  - Nickname1\n  - Nickname2\n\n# How frequently (in ms) chat should be scanned.\n# e.g. 200 -> messages will be scanned every 200ms\n# Smaller delay may improve efficiency, but will end up with higher CPU and disk usage\nscan_frequency: 300\n\n# MySQL DB Access config\n# If you want to use MySQL, uncomment all lines below and type your credentials\n# When optional is set to false, program will finish with an error when MySQL/MariaDB server is unreachable. When it is set to true, program will only warn that it can't save data to MySQL/MariaDB, but continue its work\n# If the user doesn't have password (VERY UNSAFE!), leave password field blank (nothing after ':')\n# Minimal required user permissions: CREATE, INSERT\n# WARNING: Requires mysql.connector module, which can be installed using: `pip install mysql-connector-python`\n#mysql:\n#  host: localhost\n#  port: 3306\n#  user: root\n#  password: \n#  database: my_database\n#  optional: false'''
+        DEFAULT_CONFIG_FILE=f'''# MC Code Copier default config file\n# Program maintainer: AirKeyooo <airkeyooo@gmail.com>\n# Generated: {datetime.now().astimezone().strftime("%d.%m.%Y %H:%M:%S %Z")}\n\n# Add path to your latest.log file\nlog_file: /path/to/latest.log\n\n# How many lines program should read. More lines = improved efficiency, but higher CPU and disk usage\nread_lines: 2\n\n# Should program send notifications about new codes?\n# WARNING: Works only on Linux and ends with error on any other OS!\nsend_notifications: false\n\n# Suggest when user should send code to don't look suspicious\n# Give value in seconds. If you don't want to use this function, comment it or set value to 0\nsuggest_timeout: 5\n\n# Change shuold program predict when next code will appear.\n# Program currently needs at least 2 codes in memory to predict next one\npredict_next_code: false\n\n# Should program automatically copy received code to the clipboard?\n# WARNING: Requires pyperclib module, which can be installed using: `pip install pyperclip`\ncopy_to_clipboard: false\n\n# Save results to .csv file\n# If you don't want to use this function, comment line below\nsave_to_csv: /path/to/file.csv\n\n# Set all nicknames that are yours\n# If you don't want to set your nicknames, comment/remove whole section below\nnicknames:\n  - Nickname1\n  - Nickname2\n\n# How frequently (in ms) chat should be scanned.\n# e.g. 200 -> messages will be scanned every 200ms\n# Smaller delay may improve efficiency, but will end up with higher CPU and disk usage\nscan_frequency: 300\n\n# MySQL DB Access config\n# If you want to use MySQL, uncomment all lines below and type your credentials\n# When optional is set to false, program will finish with an error when MySQL/MariaDB server is unreachable. When it is set to true, program will only warn that it can't save data to MySQL/MariaDB, but continue its work\n# If the user doesn't have password (VERY UNSAFE!), leave password field blank (nothing after ':')\n# Minimal required user permissions: CREATE, INSERT\n# WARNING: Requires mysql.connector module, which can be installed using: `pip install mysql-connector-python`\n#mysql:\n#  host: localhost\n#  port: 3306\n#  user: root\n#  password: \n#  database: my_database\n#  optional: false'''
         try:
             with open(file=fullpath.resolve(),mode="w",encoding="utf-8") as f:
                 f.write(DEFAULT_CONFIG_FILE)
@@ -403,7 +413,7 @@ class Config:
             self.defaultconf(args.config)
         # Try to read config from configuration file
         configfile=Path(args.config)
-        self.logger.debug(f"Path to configuration file loaded from parameter: {configfile.resolve()}"+" (default)"if configfile.name=="config.yml"else"")
+        self.logger.debug(f"Path to configuration file loaded from parameter: {configfile.resolve()}"+(" (default)"if configfile.name=="config.yml"else""))
         if not configfile.exists():
             self.logger.debug("Config file doesn't exist, trying to load it from other paths")
             if system()=="Linux":
@@ -533,6 +543,15 @@ class Config:
         else:
             self.suggest_timeout=0
             self.logger.debug("Program won't suggest timeout")
+        if config.get("predict_next_code"):
+            if type(config.get("predict_next_code"))is bool:
+                self.predict_next_code=config.get("predict_next_code")
+                self.logger.debug("Program will predict next code appear time")
+            else:
+                raise TypeError(f"predict_next_code can be only bool, not {config.get("predict_next_code").__class__.__name__}!")
+        else:
+            self.predict_next_code=False
+            self.logger.debug("Program won't predict next code appear time")
         self.copy_to_clipboard=bool(config.get("copy_to_clipboard"))
         self.logger.debug("Program will copy code to clipboard" if self.copy_to_clipboard else "Program won't copy code to clipboard")
         if config.get("save_to_csv"):
@@ -743,7 +762,6 @@ def main():
         stime = time()
         # Try to load code and winner info
         code = chat.get_code(n=linestoread)
-        winner = chat.get_winner(n=linestoread)
         # If code has appeared...
         if code:
             # Process, copy and display info about it
@@ -751,7 +769,7 @@ def main():
             addtots = config.suggest_timeout
             if addtots:
                 sendin = codets + timedelta(0,addtots)
-                sendtxt = f"Code {code} was found! Send it at: {sendin.hour}:{sendin.minute}:{sendin.second}"
+                sendtxt = f"Code {code} was found! Send it at: {sendin.hour:02}:{sendin.minute:02}:{sendin.second:02}"
             else:
                 sendtxt = f"Code {code} was found!"
             logger.info(sendtxt)
@@ -759,21 +777,27 @@ def main():
                 codecopy.copy(code)
             if sendnf:
                 notifications.send_notification("Code appeared",sendtxt)
-        # If winer info has appeared...
-        if winner:
-            # Process and save it
-            logger.info(winner.to_msg())
-            if savetocsv:
-                csvf.append_code_info(winner)
-            if savetomysql:
-                try:
-                    mysqlf.append_code_info(winner)
-                except RuntimeError as e:
-                    logger.warning(f"Code info wasn't saved to MySQL. MySQL error: {e}")
-                except Exception as e:
-                    logger.debug(f"Program error: {e}")
-                    logger.critical("Internal app error!")
-                    exit(rtn.EX_SOFTWARE)
+        else:
+            winner = chat.get_winner(n=linestoread)
+            if winner:
+                # Process and save winner info
+                logger.info(winner.to_msg())
+                if savetocsv:
+                    csvf.append_code_info(winner)
+                if savetomysql:
+                    try:
+                        mysqlf.append_code_info(winner)
+                    except RuntimeError as e:
+                        logger.warning(f"Code info wasn't saved to MySQL. MySQL error: {e}")
+                    except Exception as e:
+                        logger.debug(f"Program error: {e}")
+                        logger.critical("Internal app error!")
+                        exit(rtn.EX_SOFTWARE)
+                if config.predict_next_code:
+                    prediction=chat.predict_next_code()
+                    if prediction:
+                        prediction_dt=datetime.now()+timedelta(seconds=prediction)
+                        logger.info(f"Next code appear time prediction: {prediction_dt.hour:02}:{prediction_dt.minute:02}:{prediction_dt.second:02}")
         # Sleep loop
         timetosleep = (sleepms/1000)-(time()-stime)
         if timetosleep > 0:
